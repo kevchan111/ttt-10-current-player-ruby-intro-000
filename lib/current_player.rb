09bf711f6f928b53e2c2bfbccfe35c237bd790bf
Kevin Chan <kevchan111@gmail.com>
2018-04-12 19:50:37 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,11 @@
 def turn_count(board)
 return board.count {|a| a == "X" || a == "O" }
 end
+
+def current_player(board)
+  if turn_count % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end 
